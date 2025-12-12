d16ff50e0f21c779f5a8b05b1c2ae2abee535bff
SujithaMuthusamy <sujiieee.05@gmail.com>
2025-12-12 14:13:48 +0530

    Updated server fixes

--- Server/server.js
+++ Server/server.js
@@ -1,21 +1,30 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
+
 dotenv.config();
 
 import authRoutes from './routes/authRoutes.js';
 import studyPlanRoutes from './routes/studyPlanRoutes.js';
 
-import {connectDB} from './config/db.js';
+import { connectDB } from './config/db.js';
 connectDB();
 
 const app = express();
-app.use(cors(({"origin": "*"})));
+
+// CORS
+app.use(cors({ origin: "*" }));
+
+// Body parser
 app.use(express.json());
 
-app.use('/api',authRoutes);
-app.use('/api',studyPlanRoutes);
+// Routes
+app.use('/api', authRoutes);
+app.use('/api', studyPlanRoutes);
 
-app.listen(process.env.PORT , () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-})
+// PORT for Render + Local
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
